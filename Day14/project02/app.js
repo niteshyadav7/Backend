da5fe885d583de755a5d174a00a5b7a6485181d0
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-06-16 00:40:27 +0530

    file setup for manual auth

---
+++ Day14/project02/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const hostName = "http://localhost";
+const port = 8000;
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+
+const app = express();
+
+const db = require("./config/mongoose");
+
+app.use(expressLayout);
+
+app.use("/", require("./routes/index"));
+
+// app.set("layout extractsStyles", true);
+// app.set("layout extractsScripts", true);
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"), "views");
+
+app.use(express.static("./assets"));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Error", err);
+  }
+  console.log(`express running on ${hostName}:${port}`);
+});
