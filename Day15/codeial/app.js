b3c8d6b8d7f295227a87e8b5b677c1cb84828abf
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-06-18 16:41:03 +0530

    file setup

--- Day15/codeial/app.js
+++ Day15/codeial/app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const hostName = "http://localhost";
 const port = 1235;
 const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
+const db = require("./config/mongoose");
+
+app.use(expressLayouts);
+
 app.use("/", require("./routes/index"));
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"), "views");
+app.use(express.static("./public"));
 
 app.listen(port, (err) => {
   if (err) {
